ff7d3eba9ef044c93bf665e9549d416b42c4044a
dkdkdk606 <75025514+dkdkdk606@users.noreply.github.com>
2021-05-20 17:29:03 +0900

    Create 파일05쓰기06단어장.py

---
+++ 파일05쓰기06단어장.py
@@ -0,0 +1,6 @@
+#with open('new_file.txt', 'w') as f: #'a' 는 append 추가 만약 해당 파일이 없으면 만들고 있으면 추가
+    #f.write("hellow world\n")
+    #f.wirte("My name is byeongchan."
+with open('vocavulary.txt', 'a') as f:
+    input("영어단어를 입력하세요: ","text")
+    
